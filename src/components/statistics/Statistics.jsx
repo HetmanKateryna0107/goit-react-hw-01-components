56cfdea049ea4150bc6636b1553b3f287051c9aa
HetmanKateryna0107 <125737708+HetmanKateryna0107@users.noreply.github.com>
2023-05-24 15:05:01 +0300

    css2

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,6 +1,8 @@
+import css from './Statistics.module.css'
+
 export const Statistics=({title, stats})=>{
     return(
-<section className="statistics">
+<section className="{css.statistics}">
   <h2 className="title">{title}</h2>
 
   <ul className="stat-list">
